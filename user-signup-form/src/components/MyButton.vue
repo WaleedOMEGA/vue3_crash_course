ca5d611a6a96fba33c0862561abf4197848c808b
waleedomega <waleedomega@hotmail.com>
2021-02-27 04:52:56 +0200

    my button component

---
+++ user-signup-form/src/components/MyButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>
+  Button
+  </button>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
