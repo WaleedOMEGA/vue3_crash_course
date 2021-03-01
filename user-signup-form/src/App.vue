675012e8e8ddda7bab13b9e5b077d950aef7f520
WALEEDSALAH-PC\Professional <waleedomega@hotmail.com>
2021-03-01 15:58:10 +0200

    user signup form complete

--- user-signup-form/src/App.vue
+++ user-signup-form/src/App.vue
@@ -1,29 +1,75 @@
 <template>
+  <form @submit.prevent="submit" class="form">
+  <MyInput
+  name="Username"
+  :rules="{required:true,min:5}"
+  :value="username.value"
+  @update="update"
+  type="text"
+  />
+ 
+  <MyInput
+  name="Password"
+  :rules="{required:true,min:10}"
+  :value="password.value"
+  @update="update"
+  type="password"
+  />
+  
 <MyButton
 color="white"
 background="red"
-disabled="true"
-:disable="!valid"
+:disabled="!valid"
 />
+</form>
 </template>
 
 <script>
 import MyButton from './components/MyButton'
+import MyInput from './components/MyInput'
 
 export default {
  components:{
-   MyButton
+   MyButton,
+   MyInput
  },
  data(){
 return{
-  valid:false
+  username:{
+    value:'',
+    valid:false
+  },
+  password:{
+    value:'',
+    valid:false
+  }
 }
+ },methods:{
+   submit($evt){
+ 
+    console.log('submit')
+   },
+   update(payload){
+     this[payload.name.toLowerCase()]={
+       value:payload.value,
+       valid:payload.valid
+     }
+   }
+ },
+ computed:{
+   valid(){
+     return this.username.valid && this.password.valid
+   }
  }
 }
 </script>
 
 <style>
-div{
-  color:red
+body{
+  font-family: Arial;
+}
+.form{
+  max-width: 400px;
+  width:50%
 }
 </style>
