aa29a58fc4746ee59e0bcb2ac5d544d7bdd631bf
waleedomega <waleedomega@hotmail.com>
2021-03-05 22:54:42 +0200

    add controls component

--- microblog/src/App.vue
+++ microblog/src/App.vue
@@ -1,22 +1,38 @@
 <template>
-  <div></div>
+  
+  <Card v-for="post in store.state.posts">
+    <template v-slot:title>
+    {{post.title}}
+   </template>
+  <template v-slot:content>
+    {{post.content}}
+  </template>
+  <template v-slot:description>
+   <Controls />
+  </template>
+ </Card>
+ 
 </template>
 
 <script>
+import Card from './components/Card'
+import Controls from './components/Controls.vue'
+import {store} from './store.js'
 
 
 export default {
-  
+  components:{
+    Card,
+    Controls
+  },
+  setup(){
+    return {
+      store
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
