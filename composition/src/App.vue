1d81807577d6d9fb1046ecbdeedafcbddbda4fc6
waleedomega <waleedomega@hotmail.com>
2021-03-05 20:58:34 +0200

    composition complete

--- composition/src/App.vue
+++ composition/src/App.vue
@@ -1,47 +1,32 @@
 <template>
-  <button @click="increment">{{count}}</button>
-  <button @click="increase('a')">{{numbers.a}}</button>
-  <button @click="increase('b')">{{numbers.b}}</button>
-  {{total}}
+  <button @click="increment">{{ count }}</button>
+  <button @click="a++">{{ a }}</button>
+  <button @click="b++">{{ b }}</button>
+  <div v-for="msg in history">
+    {{ msg }}
+  </div>
 </template>
 
 <script>
-import {ref,reactive,computed} from 'vue'
+import { useNumbers } from './numbers'
 
 export default {
-  setup(){
-    const count = ref(0)
-    const numbers = reactive({
-      a:0,
-      b:0
-    })
-    const increase=(num)=>{
-numbers[num]+=1
-    }
-    const increment=()=>{
-      count.value+=1
-    }
-    const total=computed(()=>{
-      return count.value+numbers.a+numbers.b
-    })
-    return{
-      total,
+  setup() {
+    const {
+      history,
       increment,
-      increase,
       count,
-      numbers
+      a,
+      b
+    } = useNumbers()
+
+    return {
+      history,
+      increment,
+      count,
+      a,
+      b
     }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
